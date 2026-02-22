7b7fb815e7d8f550e3c0a1a3cb006914a68d79f4
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-22 10:43:27 +0500

    Update Armor.java

--- src/prototype/Armor.java
+++ src/prototype/Armor.java
@@ -1,4 +1,16 @@
 package prototype;
 
-public class Armor {
+public class Armor implements Cloneable {
+    public String name;
+    public int defense;
+
+    public Armor(String n, int d) {
+        name = n;
+        defense = d;
+    }
+
+    public Armor clone() {
+        try { return (Armor) super.clone(); }
+        catch (Exception e) { throw new RuntimeException(e); }
+    }
 }
