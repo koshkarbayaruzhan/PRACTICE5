e1f5a3e6be275ffd05f18683ffe3ea9a30532aa7
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-22 10:40:00 +0500

    Update LoggerMain.java

--- src/singleton/LoggerMain.java
+++ src/singleton/LoggerMain.java
@@ -1,4 +1,24 @@
-package singleton;
+package main;
+
+import singleton.*;
 
 public class LoggerMain {
+
+    public static void main(String[] args) throws Exception {
+
+        Runnable task = () -> {
+            Logger log = Logger.getInstance();
+            log.log("Thread work", LogLevel.INFO);
+            log.log("Warning detected", LogLevel.WARNING);
+            log.log("Critical error", LogLevel.ERROR);
+        };
+
+        for (int i = 0; i < 5; i++) {
+            new Thread(task).start();
+        }
+
+        Thread.sleep(1000);
+
+        LogReader.read("app.log", LogLevel.ERROR);
+    }
 }
