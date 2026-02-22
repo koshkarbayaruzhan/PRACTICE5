051962d0a0742420028b1f3988fac7386fef8a82
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-22 10:43:42 +0500

    Update Skill.java

--- src/prototype/Skill.java
+++ src/prototype/Skill.java
@@ -1,4 +1,12 @@
 package prototype;
 
-public class Skill {
+public class Skill implements Cloneable {
+    public String name;
+
+    public Skill(String n) { name = n; }
+
+    public Skill clone() {
+        try { return (Skill) super.clone(); }
+        catch (Exception e) { throw new RuntimeException(e); }
+    }
 }
