0d5b793cf3c2520324e5d1203faecdd825814171
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-22 10:42:24 +0500

    Update ReportDirector.java

--- src/builder/ReportDirector.java
+++ src/builder/ReportDirector.java
@@ -1,4 +1,12 @@
 package builder;
 
 public class ReportDirector {
+
+    public void construct(IReportBuilder b, ReportStyle s) {
+        b.setHeader("Отчет");
+        b.addSection("Продажи", "100 000");
+        b.addSection("Расходы", "40 000");
+        b.setFooter("Конец");
+        b.setStyle(s);
+    }
 }
