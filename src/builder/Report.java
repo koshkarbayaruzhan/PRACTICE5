ed760a0b789074ab3fd4f1e9238e9c2bd59ffeb9
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-22 10:40:37 +0500

    Update Report.java

--- src/builder/Report.java
+++ src/builder/Report.java
@@ -1,4 +1,18 @@
 package builder;
 
+import java.util.*;
+
 public class Report {
+
+    public String header;
+    public String footer;
+    public List<String> sections = new ArrayList<>();
+    public ReportStyle style;
+
+    public void export() {
+        System.out.println("HEADER: " + header);
+        sections.forEach(System.out::println);
+        System.out.println("FOOTER: " + footer);
+        System.out.println("STYLE: " + style.fontColor + " " + style.fontSize);
+    }
 }
