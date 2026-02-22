a9b2a46973477cd1953d064c9605df13f5f429a8
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-22 10:44:17 +0500

    Update Character.java

--- src/prototype/Character.java
+++ src/prototype/Character.java
@@ -1,4 +1,26 @@
 package prototype;
 
-public class Character {
+import java.util.*;
+
+public class Character implements Cloneable {
+
+    public int hp, str, agi, intel;
+    public Weapon weapon;
+    public Armor armor;
+    public List<Skill> skills = new ArrayList<>();
+
+    public Character clone() {
+        try {
+            Character c = (Character) super.clone();
+            c.weapon = weapon.clone();
+            c.armor = armor.clone();
+
+            c.skills = new ArrayList<>();
+            for (Skill s : skills) c.skills.add(s.clone());
+
+            return c;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
