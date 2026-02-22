37f714ddfc42c7c8436f24f5407ed83ee874dad1
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-22 10:41:15 +0500

    Update TextReportBuilder.java

--- src/builder/TextReportBuilder.java
+++ src/builder/TextReportBuilder.java
@@ -1,4 +1,13 @@
 package builder;
 
-public class TextReportBuilder {
+public class TextReportBuilder implements IReportBuilder {
+
+    private Report r = new Report();
+
+    public void setHeader(String h) { r.header = h; }
+    public void addSection(String n, String c) { r.sections.add(n + ": " + c); }
+    public void setFooter(String f) { r.footer = f; }
+    public void setStyle(ReportStyle s) { r.style = s; }
+
+    public Report getReport() { return r; }
 }
