cdd412f6c86e8a5f82b0743f10b04fc28065d8e5
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-22 10:43:09 +0500

    Update Weapon.java

--- src/prototype/Weapon.java
+++ src/prototype/Weapon.java
@@ -1,4 +1,16 @@
 package prototype;
 
-public class Weapon {
+public class Weapon implements Cloneable {
+    public String name;
+    public int damage;
+
+    public Weapon(String n, int d) {
+        name = n;
+        damage = d;
+    }
+
+    public Weapon clone() {
+        try { return (Weapon) super.clone(); }
+        catch (Exception e) { throw new RuntimeException(e); }
+    }
 }
