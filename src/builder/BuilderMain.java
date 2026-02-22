cb4f66b3836c1cc472451ad9368bba95bb69d9d8
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-22 10:42:50 +0500

    Update BuilderMain.java

--- src/builder/BuilderMain.java
+++ src/builder/BuilderMain.java
@@ -1,4 +1,17 @@
-package builder;
+package main;
+
+import builder.*;
 
 public class BuilderMain {
+
+    public static void main(String[] args) {
+
+        ReportDirector d = new ReportDirector();
+        ReportStyle style = new ReportStyle("white","black",14);
+
+        IReportBuilder b = new HtmlReportBuilder();
+        d.construct(b, style);
+
+        b.getReport().export();
+    }
 }
