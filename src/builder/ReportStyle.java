36c9bc44ee882c8bb7b07d5afe18cc594435d255
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-22 10:40:23 +0500

    Update ReportStyle.java

--- src/builder/ReportStyle.java
+++ src/builder/ReportStyle.java
@@ -1,4 +1,13 @@
 package builder;
 
 public class ReportStyle {
+    public String bgColor;
+    public String fontColor;
+    public int fontSize;
+
+    public ReportStyle(String bg, String font, int size) {
+        bgColor = bg;
+        fontColor = font;
+        fontSize = size;
+    }
 }
