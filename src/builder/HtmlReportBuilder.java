dec854af37bdee6c44518f7762ee9f4efdcabcb1
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-22 10:41:33 +0500

    Update HtmlReportBuilder.java

--- src/builder/HtmlReportBuilder.java
+++ src/builder/HtmlReportBuilder.java
@@ -1,4 +1,13 @@
 package builder;
 
-public class HtmlReportBuilder {
+public class HtmlReportBuilder implements IReportBuilder {
+
+    private Report r = new Report();
+
+    public void setHeader(String h) { r.header = "<h1>" + h + "</h1>"; }
+    public void addSection(String n, String c) { r.sections.add("<p>" + n + ": " + c + "</p>"); }
+    public void setFooter(String f) { r.footer = "<footer>" + f + "</footer>"; }
+    public void setStyle(ReportStyle s) { r.style = s; }
+
+    public Report getReport() { return r; }
 }
