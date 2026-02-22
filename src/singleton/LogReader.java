311eb9dbaca4f70ca1d388c9cc93ccd0fbd548ec
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-22 10:39:45 +0500

    Update LogReader.java

--- src/singleton/LogReader.java
+++ src/singleton/LogReader.java
@@ -1,4 +1,17 @@
 package singleton;
 
+import java.io.*;
+
 public class LogReader {
+
+    public static void read(String file, LogLevel filter) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            if (line.contains("[" + filter + "]")) {
+                System.out.println(line);
+            }
+        }
+    }
 }
